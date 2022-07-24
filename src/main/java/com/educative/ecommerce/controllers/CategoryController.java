c84b21a8458d65360625476aebcd1d67745e4e70
rvallejosbarria <rvallejosbarria@gmail.com>
2022-07-24 15:05:32 -0400

    List all Categories and Edit a Category

--- src/main/java/com/educative/ecommerce/controllers/CategoryController.java
+++ src/main/java/com/educative/ecommerce/controllers/CategoryController.java
@@ -1,5 +1,6 @@
 package com.educative.ecommerce.controllers;
 
+import java.util.List;
 import java.util.Objects;
 
 import javax.validation.Valid;
@@ -7,6 +8,8 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,5 +34,24 @@ public class CategoryController {
     }
     categoryService.createCategory(category);
     return new ResponseEntity<>(new ApiResponse(true, "created the category"), HttpStatus.CREATED);
+  }
+
+  @GetMapping("/")
+  public ResponseEntity<List<Category>> getCategories() {
+    List<Category> body = categoryService.listCategories();
+    return new ResponseEntity<>(body, HttpStatus.OK);
+  }
+
+  @PostMapping("/update/{categoryID}")
+  public ResponseEntity<ApiResponse> updateCategory(@PathVariable("categoryID") Integer categoryID, @Valid @RequestBody Category category) {
+    // Check to see if the category exists.
+    if (Objects.nonNull(categoryService.readCategory(categoryID))) {
+      // If the category exists then update it.
+      categoryService.updateCategory(categoryID, category);
+      return new ResponseEntity<>(new ApiResponse(true, "updated the category"), HttpStatus.OK);
+    }
+
+    // If the category doesn't exist then return a response of unsuccessful.
+    return new ResponseEntity<>(new ApiResponse(false, "category does not exist"), HttpStatus.NOT_FOUND);
   }
 }
--- src/main/java/com/educative/ecommerce/service/CategoryService.java
+++ src/main/java/com/educative/ecommerce/service/CategoryService.java
@@ -1,5 +1,8 @@
 package com.educative.ecommerce.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +19,22 @@ public class CategoryService {
   }
 
   public void createCategory(Category category) {
+    categoryRepository.save(category);
+  }
+
+  public List<Category> listCategories() {
+    return categoryRepository.findAll();
+  }
+
+  public Optional<Category> readCategory(Integer categoryID) {
+    return categoryRepository.findById(categoryID);
+  }
+
+  public void updateCategory(Integer categoryID, Category newCategory) {
+    Category category = categoryRepository.findById(categoryID).get();
+    category.setCategoryName(newCategory.getCategoryName());
+    category.setDescription(newCategory.getDescription());
+    category.setImageUrl(newCategory.getImageUrl());
     categoryRepository.save(category);
   }
 }
