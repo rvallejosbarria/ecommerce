74ff07baf405ccb2ad3096d19fe6c191811af428
rvallejosbarria <rvallejosbarria@gmail.com>
2022-08-28 17:47:55 -0400

    Product API: Create a New Product

---
+++ src/main/java/com/educative/ecommerce/dto/ProductDto.java
@@ -0,0 +1,81 @@
+package com.educative.ecommerce.dto;
+
+import com.educative.ecommerce.model.Product;
+
+import javax.validation.constraints.NotNull;
+
+public class ProductDto {
+    private Integer id;
+    private @NotNull String name;
+    private @NotNull String imageUrl;
+    private @NotNull double price;
+    private @NotNull String description;
+    private @NotNull Integer categoryId;
+
+    public ProductDto(@NotNull String name, @NotNull String imageUrl, @NotNull double price, @NotNull String description, @NotNull Integer categoryId) {
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.price = price;
+        this.description = description;
+        this.categoryId = categoryId;
+    }
+
+    public ProductDto(Product product) {
+        this.setId(product.getId());
+        this.setName(product.getName());
+        this.setImageUrl(product.getImageUrl());
+        this.setDescription(product.getDescription());
+        this.setCategoryId(product.getCategory().getId());
+    }
+
+    public ProductDto() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+}
---
+++ src/main/java/com/educative/ecommerce/service/ProductService.java
@@ -0,0 +1,29 @@
+package com.educative.ecommerce.service;
+
+import com.educative.ecommerce.dto.ProductDto;
+import com.educative.ecommerce.model.Category;
+import com.educative.ecommerce.model.Product;
+import com.educative.ecommerce.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    public void addProduct(ProductDto productDto, Category category) {
+        Product product = getProductFromDto(productDto, category);
+        productRepository.save(product);
+    }
+
+    public static Product getProductFromDto(ProductDto productDto, Category category) {
+        Product product = new Product();
+        product.setCategory(category);
+        product.setDescription(productDto.getDescription());
+        product.setImageUrl(productDto.getImageUrl());
+        product.setPrice(productDto.getPrice());
+        product.setName(productDto.getName());
+        return product;
+    }
+}
